3710e49cc7de0de232421b36f7539dc691f78b73
Nana Axel <ax.lnana@outlook.com>
2016-10-24 07:13:47 +0200

    Create the API socket server This class will manage incoming connections from the web administration interface

---
+++ jsondb-server/APISocketServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace JSONDB.Server
+{
+    class APISocketServer : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            /// TODO: Implement this method
+            base.OnMessage(e);
+        }
+    }
+}
