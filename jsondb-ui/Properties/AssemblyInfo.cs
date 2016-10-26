5a2c3bb386b89b24eb36f3e7d645ab3dce48df0c
Nana Axel <ax.lnana@outlook.com>
2016-10-27 01:47:32 +0200

    Update project properties, adding ressources and settings managers

--- jsondb-ui/Properties/AssemblyInfo.cs
+++ jsondb-ui/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JSONDB Server Manager")]
-[assembly: AssemblyDescription("Start or stop the server.")]
+[assembly: AssemblyDescription("Manage and configure the JSONDB Server.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JSONDB Server")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-CM")]
+
