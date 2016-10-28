c2b94e79aeee135d4d1e5ec0a239be3fe2daa2d7
Nana Axel <ax.lnana@outlook.com>
2016-10-28 02:59:23 +0200

    Add a cache manager to reduce number of read/write operations on tables

---
+++ jsondb/Cache.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JSONDB
+{
+    class Cache
+    {
+        /// <summary>
+        /// An array of all cached data.
+        /// </summary>
+        private static Dictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Retrieve a cached table data.
+        /// </summary>
+        /// <param name="path">The path to the table</param>
+        /// <returns>The cached data</returns>
+        internal static string Get(string path)
+        {
+            if (!Data.ContainsKey(path))
+            {
+                Data[path] = Database.GetTableData(path).ToString();
+            }
+
+            return Data[path].ToString();
+        }
+
+        /// <summary>
+        /// Update cached table data.
+        /// </summary>
+        /// <param name="path">The path to the table</param>
+        /// <param name="data">The data to cache</param>
+        internal static void Update(string path, string data)
+        {
+            if (data == String.Empty)
+            {
+                Data[path] = Database.GetTableData(path).ToString();
+            }
+            else
+            {
+                Data[path] = data;
+            }
+        }
+
+        /// <summary>
+        /// Reset all the cache.
+        /// </summary>
+        internal static void Reset()
+        {
+            Data = new Dictionary<string, string>();
+        }
+    }
+}
