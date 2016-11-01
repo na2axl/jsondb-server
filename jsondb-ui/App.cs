a0c879f441710d3403edc324b0e3300df641d8b1
Nana Axel <ax.lnana@outlook.com>
2016-11-01 08:59:33 +0100

    Update application main logic * Only one instance of application is allowed * Add a method used to run an elvated client for operations which demands admin rights

--- jsondb-ui/App.cs
+++ jsondb-ui/App.cs
@@ -1,20 +1,40 @@
 ﻿using System.Windows;
 using System.Diagnostics;
+using Microsoft.Win32;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace JSONDB.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private static Process ServerProcess;
 
         private static AppSettings Settings = new AppSettings();
 
+        static Mutex mutex = new Mutex(true, ResourceAssembly.FullName);
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+                mutex.ReleaseMutex();
+            }
+        }
+
+        public void InitializeComponent()
+        {
+            StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+        }
+
         public static void StartServer()
         {
-            string serverCommandLine = "";
+            string serverCommandLine = String.Empty;
 
             if (Settings.UseCustomServerAdress)
             {
@@ -66,6 +86,24 @@ namespace JSONDB.UI
         public static bool ServerIsStopped()
         {
             return (null == ServerProcess || ServerProcess.HasExited);
+        }
+
+        public static void RunElevatedClient(string command)
+        {
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo StartInfo = new ProcessStartInfo();
+                StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                StartInfo.CreateNoWindow = true;
+                StartInfo.FileName = "jsondb-elevated-client.exe";
+                StartInfo.Arguments = command;
+                StartInfo.Verb = "runas";
+                StartInfo.UseShellExecute = true;
+                process.StartInfo = StartInfo;
+                process.Start();
+            }
+            catch { /* Operation surely cancelled by the user... */ }
         }
     }
 }
