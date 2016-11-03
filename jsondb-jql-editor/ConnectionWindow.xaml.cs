c6e62b75dc49f8491268533b93ccb4f18c7b3716
Nana Axel <ax.lnana@outlook.com>
2016-11-03 12:03:22 +0100

    Add a connection window

---
+++ jsondb-jql-editor/ConnectionWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JSONDB.JQLEditor
+{
+    /// <summary>
+    /// Interaction logic for ConnectionWindow.xaml
+    /// </summary>
+    public partial class ConnectionWindow : Window
+    {
+        public ConnectionWindow(Window o)
+        {
+            // Set the owner window
+            Owner = o;
+
+            // Initialize UI
+            InitializeComponent();
+
+            // Set button behavior
+            ConnectButton.IsDefault = true;
+            CancelButton.IsCancel = true;
+        }
+
+        private void ConnectToServer(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                App.Connect(ServerNameBox.Text, UsernameBox.Text, PasswordBox.Password);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                new MessageWindow(
+                    this,
+                    ex.Message,
+                    "An error occurred",
+                    MessageWindowButton.OK,
+                    MessageWindowImage.Error).Open();
+            }
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
