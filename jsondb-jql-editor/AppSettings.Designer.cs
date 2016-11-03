b4aae0e246c38b499c17436e82184e9d0c201312
Nana Axel <ax.lnana@outlook.com>
2016-11-03 12:07:33 +0100

    Add an user setting

--- jsondb-jql-editor/AppSettings.Designer.cs
+++ jsondb-jql-editor/AppSettings.Designer.cs
@@ -22,5 +22,17 @@ namespace JSONDB.JQLEditor {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ShowLineNumbers {
+            get {
+                return ((bool)(this["ShowLineNumbers"]));
+            }
+            set {
+                this["ShowLineNumbers"] = value;
+            }
+        }
     }
 }
