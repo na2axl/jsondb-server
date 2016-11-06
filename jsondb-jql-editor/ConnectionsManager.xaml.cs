18cac1f47cac87056f53366b05ed54da473540e2
Nana Axel <ax.lnana@outlook.com>
2016-11-06 02:18:22 +0100

    Create a window used to add connections

--- jsondb-jql-editor/ConnectionsManager.xaml.cs
+++ jsondb-jql-editor/ConnectionsManager.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,68 @@ namespace JSONDB.JQLEditor
     /// </summary>
     public partial class ConnectionsManager : Window
     {
-        public ConnectionsManager()
+        private AppSettings Settings;
+
+        public ConnectionsManager(Window o)
         {
+            // Set the owner
+            Owner = o;
+
+            // Load Application Settings
+            Settings = new AppSettings();
+
+            // Initialize UI
             InitializeComponent();
+
+            // Load Icons
+            ButtonAddConnection.Source = BitmapToImageSource(AppResources.AddConnectionIcon);
+            ButtonDeleteConnection.Source = BitmapToImageSource(AppResources.DeleteConnectionIcon);
+            ButtonEditConnection.Source = BitmapToImageSource(AppResources.EditConnectionIcon);
+
+            // Load connections list
+            ConnectionsList.Items.Clear();
+            foreach (JObject item in Settings.Connections)
+            {
+                ConnectionEntry entry = new ConnectionEntry(item);
+            }
+        }
+
+        private ImageSource BitmapToImageSource(System.Drawing.Bitmap bmp)
+        {
+            MemoryStream memory = new MemoryStream();
+            BitmapImage imageSource = new BitmapImage();
+
+            bmp.Save(memory, AppResources.MessageWindowError.RawFormat);
+            memory.Position = 0;
+            imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.StreamSource = memory;
+            imageSource.CacheOption = BitmapCacheOption.OnLoad;
+            imageSource.EndInit();
+            memory.Close();
+
+            return imageSource;
+        }
+
+        private class ConnectionEntry : ListBoxItem
+        {
+            private JObject _data;
+
+            public JObject Entry
+            {
+                get { return _data; }
+            }
+
+            public ConnectionEntry(JObject entry)
+            {
+                _data = entry;
+                Content = entry["name"];
+            }
+        }
+
+        private void AddConnection(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
