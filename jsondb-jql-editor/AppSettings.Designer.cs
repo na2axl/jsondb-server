0da34fa8b0723af69825c44d7d9ce540081781b0
Nana Axel <ax.lnana@outlook.com>
2016-11-06 03:23:09 +0100

    Change setting type and default value

--- jsondb-jql-editor/AppSettings.Designer.cs
+++ jsondb-jql-editor/AppSettings.Designer.cs
@@ -49,9 +49,11 @@ namespace JSONDB.JQLEditor {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        public global::Newtonsoft.Json.Linq.JArray Connections {
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
+            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
+        public global::System.Collections.Specialized.StringCollection Connections {
             get {
-                return ((global::Newtonsoft.Json.Linq.JArray)(this["Connections"]));
+                return ((global::System.Collections.Specialized.StringCollection)(this["Connections"]));
             }
             set {
                 this["Connections"] = value;
