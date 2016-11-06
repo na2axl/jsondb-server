b8708163de3cbbf1042f8942d0f516c0602bbbc0
Nana Axel <ax.lnana@outlook.com>
2016-11-06 03:32:13 +0100

    Update window, functional state

--- jsondb-jql-editor/AddConnectionWindow.xaml.cs
+++ jsondb-jql-editor/AddConnectionWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JSONDB.Library;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace JSONDB.JQLEditor
 {
@@ -19,9 +10,59 @@ namespace JSONDB.JQLEditor
     /// </summary>
     public partial class AddConnectionWindow : Window
     {
-        public AddConnectionWindow()
+        public AddConnectionWindow(Window o)
         {
+            // Set the owner
+            Owner = o;
+
+            // Initialize UI
             InitializeComponent();
+        }
+
+        private void ConnectToServer(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Database temp_db = Library.JSONDB.Connect(ServerNameBox.Text, UsernameBox.Text, PasswordBox.Password, DatabaseNameBox.Text);
+                if (temp_db.IsConnected())
+                {
+                    App.Settings.Connections.Add(
+                        ConnectionNameBox.Text + "{{s}}" +
+                        ServerNameBox.Text + "{{s}}" +
+                        UsernameBox.Text + "{{s}}" +
+                        PasswordBox.Password + "{{s}}" + 
+                        DatabaseNameBox.Text);
+
+                    App.Settings.Save();
+
+                    new MessageWindow(
+                        this,
+                        "Connection added successfully.",
+                        "Connection added",
+                        MessageWindowButton.OK,
+                        MessageWindowImage.Success).Open();
+
+                    Close();
+                }
+                else
+                {
+                    throw new Exception("An error occurred when trying to connect to this server.");
+                }
+            }
+            catch (Exception ex)
+            {
+                new MessageWindow(
+                    this,
+                    ex.Message,
+                    "Error",
+                    MessageWindowButton.OK,
+                    MessageWindowImage.Error).Open();
+            }
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
