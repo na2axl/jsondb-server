f0da2aef4e0fe5c6bde6e65ddf02f2dbc25ca606
Nana Axel <ax.lnana@outlook.com>
2016-12-05 01:38:30 +0100

    Improve the socket server: * Log operations with connection IDs * Add comments to classes and methods

--- jsondb-server/ClientSocketServer.cs
+++ jsondb-server/ClientSocketServer.cs
@@ -1,24 +1,47 @@
-﻿using JSONDB.Library;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using WebSocketSharp;
 using WebSocketSharp.Net.WebSockets;
 using WebSocketSharp.Server;
 
 namespace JSONDB.Server
 {
-    public class ClientSocketServer : WebSocketBehavior
+    /// <summary>
+    /// Socket server used to execute incomming queries.
+    /// </summary>
+    internal class ClientSocketServer : WebSocketBehavior
     {
 
+        /// <summary>
+        /// List of queries to execute with FIFO method.
+        /// </summary>
         internal static Queue<QueryPool> Pools = new Queue<QueryPool>();
 
+        /// <summary>
+        /// Logger.
+        /// </summary>
+        private static readonly Logger Logger = new Logger();
+
+        /// <summary>
+        /// Occur when a new connection is opened by the client.
+        /// </summary>
+        protected override void OnOpen()
+        {
+            Logger.Log("New Connection with ID: " + ID, Logger.LogType.Info);
+        }
+
+        /// <summary>
+        /// Occur when a new message (query) is sent by the client.
+        /// </summary>
+        /// <param name="e">The message event.</param>
         protected override void OnMessage(MessageEventArgs e)
         {
+            Logger.Log("Incomming message with ID: " + ID, Logger.LogType.Info);
             if (e.IsText)
             {
-                Pools.Enqueue(new QueryPool(e.Data, Context));
+                Pools.Enqueue(new QueryPool(e.Data, Context, ID));
+                Logger.Log("Query enqueued with ID: " + ID, Logger.LogType.Info);
             }
             else
             {
@@ -26,27 +49,46 @@ namespace JSONDB.Server
             }
         }
 
+        /// <summary>
+        /// Occur when the connection is closed by the server.
+        /// </summary>
+        /// <param name="e">The close event.</param>
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Logger.Log("Connection " + ID + " closed: " + e.Reason, Logger.LogType.Info);
+        }
+
+        /// <summary>
+        /// Occur when an error occured while the client is connected.
+        /// </summary>
+        /// <param name="e">The error event.</param>
         protected override void OnError(ErrorEventArgs e)
         {
-            JObject res = new JObject();
-            res["_jdb_error"] = true;
-            res["_error_msg"] = e.Message;
-            res["_query_result"] = new JObject();
+            var res = new JObject
+            {
+                ["error"] = true,
+                ["result"] = e.Message
+            };
             SendAsync(res.ToString(), (success) =>
             {
                 Context.WebSocket.Close(CloseStatusCode.ServerError, e.Message);
             });
         }
 
+        /// <summary>
+        /// Nested class used to manage the execution of queries.
+        /// </summary>
         internal class QueryPool
         {
-            private string _query;
-            private WebSocketContext _context;
+            private readonly string _query;
+            private readonly WebSocketContext _context;
+            private readonly string _id;
 
-            public QueryPool(string query, WebSocketContext context)
+            public QueryPool(string query, WebSocketContext context, string id)
             {
                 _query = query;
                 _context = context;
+                _id = id;
             }
 
             /// <summary>
@@ -54,16 +96,16 @@ namespace JSONDB.Server
             /// </summary>
             public void Send()
             {
-                var ServerName = _context.Headers["jdb-server-name"] ?? String.Empty;
-                if (ServerName == String.Empty)
+                var serverName = _context.Headers["jdb-server-name"];
+                if (string.IsNullOrEmpty(serverName))
                 {
                     _context.WebSocket.Close(CloseStatusCode.InvalidData, "The request has not a server to connect on.");
                 }
 
                 else
                 {
-                    var DatabaseName = _context.Headers["jdb-database-name"] ?? String.Empty;
-                    if (DatabaseName == String.Empty)
+                    var databaseName = _context.Headers["jdb-database-name"];
+                    if (string.IsNullOrEmpty(databaseName))
                     {
                         _context.WebSocket.Close(CloseStatusCode.InvalidData, "The request has not a database to connect on.");
                     }
@@ -71,16 +113,14 @@ namespace JSONDB.Server
                     {
                         try
                         {
-                            Database _db = Library.JSONDB.Connect(ServerName, _context.Headers["authorization"].Replace("Basic ", ""));
-                            _db.SetDatabase(DatabaseName);
-                            JObject res = new JObject();
-                            res["_jdb_error"] = false;
-                            res["_error_msg"] = String.Empty;
-                            res["_query_result"] = _db.Query(_query);
+                            var db = Jsondb.Connect(serverName, _context.Headers["authorization"].Replace("Basic ", ""));
+                            db.SetDatabase(databaseName);
+                            var res = db.Query(_query);
                             _context.WebSocket.SendAsync(res.ToString(), (success) =>
                             {
                                 if (success)
                                 {
+                                    Logger.Log("Query executed with ID: " + _id, Logger.LogType.Info);
                                     _context.WebSocket.Close(CloseStatusCode.Normal, "Received query executed.");
                                 }
                                 else
