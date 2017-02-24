413b9df7aecdec36791f17863fbbe3a634e0482a
Nana Axel <ax.lnana@outlook.com>
2017-02-24 22:00:59 +0100

    Code optimization and commenting

--- jsondb/Benchmark.cs
+++ jsondb/Benchmark.cs
@@ -1,29 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
-namespace JSONDB.Library
+namespace JSONDB
 {
-    struct BenchPoint
+    /// <summary>
+    /// A benchmark point.
+    /// </summary>
+    internal struct BenchPoint
     {
         public long Time { get; set; }
         public long Memory { get; set; }
     }
 
-    class Benchmark
+    /// <summary>
+    /// Class Benchmark.
+    /// </summary>
+    internal static class Benchmark
     {
-        private static Dictionary<string, BenchPoint> Marker = new Dictionary<string, BenchPoint>();
+        /// <summary>
+        /// The list of bench points.
+        /// </summary>
+        private static Dictionary<string, BenchPoint> _marker = new Dictionary<string, BenchPoint>();
 
+        /// <summary>
+        /// Create a new benchpoint.
+        /// </summary>
+        /// <param name="name">The name of the benchpoint.</param>
         public static void Mark(string name)
         {
-            BenchPoint Point = new BenchPoint();
-            Point.Time = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
-            Point.Memory = System.Diagnostics.Process.GetCurrentProcess().PeakWorkingSet64;
+            var point = new BenchPoint
+            {
+                Time = Util.TimeStamp,
+                Memory = System.Diagnostics.Process.GetCurrentProcess().PeakWorkingSet64
+            };
 
-            if (Marker == null) Marker = new Dictionary<string, BenchPoint>();
+            if (_marker == null) _marker = new Dictionary<string, BenchPoint>();
 
-            Marker[name] = Point;
+            _marker[name] = point;
         }
 
+        /// <summary>
+        /// Calculate the elapsed time between two benchpoints.
+        /// </summary>
+        /// <param name="point1">The name of the first benchpoint.</param>
+        /// <param name="point2">The name of the second benchpoint.</param>
+        /// <returns>The elapsed time.</returns>
         public static long ElapsedTime(string point1, string point2)
         {
             if (point1 == null)
@@ -31,21 +53,27 @@ namespace JSONDB.Library
                 return 0;
             }
 
-            if (!Marker.ContainsKey(point1))
+            if (!_marker.ContainsKey(point1))
             {
                 Mark(point1);
             }
-            if (!Marker.ContainsKey(point2))
+            if (!_marker.ContainsKey(point2))
             {
                 Mark(point2);
             }
 
-            long s = Marker[point1].Time;
-            long e = Marker[point2].Time;
+            long s = _marker[point1].Time;
+            long e = _marker[point2].Time;
 
             return e - s;
         }
 
+        /// <summary>
+        /// Calculate the amount of memory used between two benchpoints.
+        /// </summary>
+        /// <param name="point1">The name of the first benchpoint.</param>
+        /// <param name="point2">The name of the second benchpoint.</param>
+        /// <returns>The amount of memory used.</returns>
         public static long MemoryUsage(string point1, string point2)
         {
             if (point1 == null)
@@ -53,19 +81,27 @@ namespace JSONDB.Library
                 return 0;
             }
 
-            if (!Marker.ContainsKey(point1))
+            if (!_marker.ContainsKey(point1))
             {
                 Mark(point1);
             }
-            if (!Marker.ContainsKey(point2))
+            if (!_marker.ContainsKey(point2))
             {
                 Mark(point2);
             }
 
-            long s = Marker[point1].Memory;
-            long e = Marker[point2].Memory;
+            long s = _marker[point1].Memory;
+            long e = _marker[point2].Memory;
 
             return e - s;
+        }
+
+        /// <summary>
+        /// Clean al benchpoints and reset the benchmark.
+        /// </summary>
+        public static void Reset()
+        {
+            _marker = new Dictionary<string, BenchPoint>();
         }
     }
 }
